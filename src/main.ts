59e40aaffa877423478428ed7a9f1ee6c0cb2fdf
piintegrated <piintegratedservices1@gmail.com>
2024-10-05 10:17:40 +0100

    optimised jobservice

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from your frontend
+  });
+  await app.listen(3001);
 }
 bootstrap();

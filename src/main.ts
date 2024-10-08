cb33220c768dc86d999579e5ba3eb995126d473b
Vulcanric <jneric49@gmail.com>
2024-10-08 15:35:08 +0100

    Fixed CORS error: allowed connection from 127.0.0.1:3000 too

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from your frontend
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'] // Allow requests from your frontend
   });
   await app.listen(3001);
 }

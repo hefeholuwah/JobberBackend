4fc56af716b1eb7ef7aa01d50a9f79382f83acff
piintegrated <piintegratedservices1@gmail.com>
2024-10-13 15:28:40 +0100

    added domain to CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'] // Allow requests from your frontend
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://jobber-or6u6ghop-hefeholuwahs-projects.vercel.app/',
+    ], // Allow requests from your frontend
   });
   await app.listen(3001);
 }
